4f00c4aad73387385d1daa6e7e1f36dd63d0a8ef
Hamza Sallam <hsallam278@gmail.com>
2026-08-19 00:32:44 +0300

    Solve ex 9 and 10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(small, big) {
+    let sum = 0;
+    let i = big;
+    while(i>= small){
+        sum += i;
+        i--;
+    }
+    return sum;
 };
 
 // Do not edit below this line
