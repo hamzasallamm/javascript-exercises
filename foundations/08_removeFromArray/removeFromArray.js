ad38f69643776e49c891fc964cfabfb25d458dfe
Hamza Sallam <hsallam278@gmail.com>
2026-08-19 00:05:21 +0300

    Solve ex 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+    const length = arr.length;
+    let i=0;
+    let newArr = [];
+    while(i<length){
+        if(arr[i]!==num){
+            newArr.push(arr[i]);
+        }
+        i++;
+    }
+    return newArr;
 };
 
 // Do not edit below this line
